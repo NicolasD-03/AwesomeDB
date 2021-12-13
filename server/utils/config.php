8f8d6db302df729afddcc4d382a811c6e34be310
Nicolas DICK <dick.nicolas57@gmail.com>
2021-12-14 00:05:53 +0100

    feat: Set-up config for the project
    Set-up the project config, load .env file values to global variable

---
+++ server/utils/config.php
@@ -0,0 +1,11 @@
+<?php
+$dotenv = Dotenv\Dotenv::createImmutable(getcwd(), '.env.local');
+$dotenv->load();
+
+$apiRoute = $_ENV['API_ROUTE'];
+$apiVersion = $_ENV['API_VERSION'];
+$dbType = $_ENV['DB_TYPE'];
+$dbHost = $_ENV['DB_HOST'];
+$dbUsername = $_ENV['DB_USERNAME'];
+$dbPassword = $_ENV['DB_PASSWORD'];
+$dbName = $_ENV['DB_NAME'];
