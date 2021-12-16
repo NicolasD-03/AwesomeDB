4f30f9692f390ae8aca563d1e2eda69739e2cc5c
DICK Nicolas <dick.nicolas57@gmail.com>
2021-12-16 11:07:43 +0100

    feat: Adding db connection & Route
    Adding database connection and films route with filmcontroller

--- server/index.php
+++ server/index.php
@@ -1,12 +1,30 @@
 <?php
+
+use Api\Controller\FilmController;
+use Utils\Database;
+
 require 'vendor/autoload.php';
 require 'utils/showErrors.php';
 require 'utils/config.php';
 
-$router = new AltoRouter();
+// Set requests return to json only
+header('content-type: application/json');
 
-$router->map('GET|POST|PATCH|DELETE', $apiRoute . $apiVersion . '/films', function () {
-    require 'api/routes/FilmsRoutes.php';
+// Verify db connection
+try {
+    $db = new Database($dbType, $dbHost, $dbName, $dbUsername, $dbPassword);
+} catch (Exception $e) {
+    header($_SERVER["SERVER_PROTOCOL"] . " 503 Service Unavailable", true, 503);
+    exit();
+}
+
+$router = new AltoRouter();
+$GLOBALS['filmController'] = new FilmController($db);
+
+$baseRoute = $apiRoute . $apiVersion;
+
+$router->map('GET', $baseRoute . '/films', function () {
+    $GLOBALS['filmController']->getFilms();
 });
 
 $match = $router->match();
@@ -14,5 +32,5 @@ $match = $router->match();
 if (is_array($match) && is_callable($match['target'])) {
     call_user_func_array($match['target'], $match['params']);
 } else {
-    header("HTTP/1.0 404 Not Found");
+    header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found", true, 404);
 }
