3724427490619a146aed1ca297c5e448dda52699
Nicolas DICK <dick.nicolas57@gmail.com>
2022-01-16 21:29:43 +0100

    feat: LAyout and head
    Add head and layout
    
    BREAKING CHANGE: Adding layout and head

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Layout from "../components/layout";
+import Head from "next/head";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
-
-export default MyApp
